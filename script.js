85b9332a03e2fbc54a978316b6fefbd534a2984d
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-10-08 15:33:31 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const paths=["Cloud" ,"Desktop" ,"Video Sport" ,"Mobile" ,"Web"]
+
+		const list=document.getElementById("list");
+		paths.forEach((paths)=>{
+			const li=document.createElement("li");
+			li.textContent=`${paths} development`;
+			list.appendChild(li);
+		});
+</script>
